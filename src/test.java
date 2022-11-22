dc70b058ff97d09f94d0246c70c042828e9d4a9a
so1761 <xoehfdl8182@gmail.com>
2022-11-22 23:04:12 +0900

    Update test.java

--- src/test.java
+++ src/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("My name is Park Tae Jung");
+        System.out.println("Test 1");
     }
 }
