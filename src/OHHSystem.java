a84838671f2f8070c6eee93a84995f92145b6cb8
so1761 <xoehfdl8182@gmail.com>
2022-11-24 19:07:25 +0900

    Import Swings

--- src/OHHSystem.java
+++ src/OHHSystem.java
@@ -1,6 +1,34 @@
-public class OHHSystem {
-    public static void main(String[] args) {
-        System.out.println("test main");
-        System.out.println("Park Tae Jung");
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.table.DefaultTableModel;
+
+
+
+
+public class OHHSystem extends JFrame implements ActionListener {
+    
+	
+	public void OOHSystem() {
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
     }
+	
+	public void actionPerformed(ActionEvent e) {
+		String ActionCmd = e.getActionCommand();
+	}
 }
