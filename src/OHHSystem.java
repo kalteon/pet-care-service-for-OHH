36f48b1f083ffed838d69290b33955f881eb1efb
jungjihyung <wlgud3936@gmail.com>
2022-11-24 20:38:34 +0900

    hi를 추가했습니다

--- src/OHHSystem.java
+++ src/OHHSystem.java
@@ -2,5 +2,6 @@ public class OHHSystem {
     public static void main(String[] args) {
         System.out.println("test main");
         System.out.println("Park Tae Jung");
+        System.out.println("Hi");
     }
 }
