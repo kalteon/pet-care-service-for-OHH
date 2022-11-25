847742e64351029bc98b964d7ce043b49c320198
so1761 <xoehfdl8182@gmail.com>
2022-11-25 20:37:14 +0900

    로그인 / 회원 UI에 디자인 추가

--- src/hci/LoginUI.java
+++ src/hci/LoginUI.java
@@ -1,62 +1,65 @@
 package hci;
 
-import java.util.ArrayList;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Color;
 import java.awt.Font;
 
-import javax.swing.JButton;
+
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JTable;
-import javax.swing.JScrollPane;
-import javax.swing.JTextField;
-import javax.swing.UIManager;
-import javax.swing.JComboBox;
-import javax.swing.table.DefaultTableModel;
+
 
 @SuppressWarnings("serial")
 public class LoginUI extends JFrame implements ActionListener {
 	
+	Color c;
+	
 	public LoginUI() {
 		
 		super("LoginUI");
-		try {
-		    UIManager.setLookAndFeel("javax.swing.plaf.mac.MacLookAndFeel");
-		} catch (Exception e) {
-		    /*
-		        ClassNotFoundException
-		        InstantiationException
-		        IllegalAccessException
-		        UnsupportedLookAndFeelException
-		     */
-		}
 		setSize(600, 800);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		setLayout(null);
+				
+		c = new Color(188,204,250);
+		getContentPane().setBackground(c);
+
 		
+		/*
 		JLabel TitleLabel = new JLabel("로그인 선택");
 		TitleLabel.setHorizontalAlignment(JLabel.CENTER);
 		TitleLabel.setFont(new Font("맑은 고딕", Font.BOLD, 50));
 		add(TitleLabel);
 		TitleLabel.setBounds(50, 50, 500, 70);
+		*/
 		
-		JButton MemberLoginButton = new JButton("회원 로그인");
+		RoundedButton MemberLoginButton = new RoundedButton("회원 로그인");
 		add(MemberLoginButton);
-		MemberLoginButton.setBounds(100, 300, 150, 150);
-		MemberLoginButton.setFont(new Font("맑은 고딕", Font.BOLD, 15));
+		c = new Color(64,126,219);
+		MemberLoginButton.setBackground(c);
+		MemberLoginButton.setForeground(Color.WHITE);
+		MemberLoginButton.setBounds(30, 550, 530, 50);
+		MemberLoginButton.setFont(new Font("맑은 고딕", Font.BOLD, 20));
 		MemberLoginButton.addActionListener(this);
 		
-		JButton PetSitterLoginButton = new JButton("돌봄이 로그인");
+		
+		RoundedButton PetSitterLoginButton = new RoundedButton("돌봄이 로그인");
 		add(PetSitterLoginButton);
-		PetSitterLoginButton.setBounds(350, 300, 150, 150);
-		PetSitterLoginButton.setFont(new Font("맑은 고딕", Font.BOLD, 15));
+		c = new Color(64,126,219);
+		PetSitterLoginButton.setBackground(c);
+		PetSitterLoginButton.setForeground(Color.WHITE);
+		PetSitterLoginButton.setBounds(30, 610, 530, 50);
+		PetSitterLoginButton.setFont(new Font("맑은 고딕", Font.BOLD, 20));
 		PetSitterLoginButton.addActionListener(this);
+		
+		RoundedButton ExitButton = new RoundedButton("종료하기");
+		add(ExitButton);
+		ExitButton.setBackground(Color.WHITE);
+		ExitButton.setBounds(30, 670, 530, 50);
+		ExitButton.setFont(new Font("맑은 고딕", Font.BOLD, 20));
+		ExitButton.addActionListener(this);
 		
 	}
 	
@@ -72,9 +75,18 @@ public class LoginUI extends JFrame implements ActionListener {
 			PetSitterWindow.setVisible(true);
 			dispose();
 		}
+		else if(ActionCmd.equals("종료하기")) {
+			int ans = JOptionPane.showConfirmDialog(null,"정말 종료하시겠습니까?","확인 메세지",JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE);
+			if(ans == 0){ // 종료하기
+				System.exit(0);
+			}
+			
+		}
 		else {
 			System.out.println("Unexpected Error");
 			System.exit(0);
 		}
 	}
+	
+	
 }
--- src/hci/MemberUI.java
+++ src/hci/MemberUI.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Graphics;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -14,12 +15,17 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.table.DefaultTableModel;
 
 @SuppressWarnings("serial")
 public class MemberUI extends JFrame implements ActionListener{
+	
+	Color c;
+	String name = "박태정";
+	
 	public MemberUI() {
 		super("MemberUI");
 		setSize(600, 800);
@@ -27,23 +33,50 @@ public class MemberUI extends JFrame implements ActionListener{
 		setLocationRelativeTo(null);
 		setLayout(null);
 		
-		JLabel TitleLabel = new JLabel("회원 기능");
-		TitleLabel.setHorizontalAlignment(JLabel.CENTER);
+		c = new Color(188,204,250);
+		getContentPane().setBackground(c);
+		
+		JLabel TitleLabel = new JLabel("안녕하세요!");
+		TitleLabel.setHorizontalAlignment(JLabel.LEFT);
+		TitleLabel.setForeground(Color.BLACK);
 		TitleLabel.setFont(new Font("맑은 고딕", Font.BOLD, 50));
 		add(TitleLabel);
-		TitleLabel.setBounds(50, 50, 500, 70);
+		TitleLabel.setBounds(30, 50, 500, 70);
 		
-		JButton ApplicationButton = new JButton("신청하기");
+		JLabel NameLabel = new JLabel(name + "님");
+		NameLabel.setHorizontalAlignment(JLabel.LEFT);
+		c = new Color(103,155,245);
+		NameLabel.setForeground(Color.BLACK);
+		NameLabel.setFont(new Font("맑은 고딕", Font.BOLD, 50));
+		add(NameLabel);
+		NameLabel.setBounds(320, 50, 500, 70);
+		
+		JSeparator JSep = new JSeparator();
+		add(JSep);
+		JSep.setBounds(0, 170, 600, 70);
+		
+		
+		RoundedButton ApplicationButton = new RoundedButton("돌봄 서비스 신청하기");
 		add(ApplicationButton);
-		ApplicationButton.setBounds(100, 300, 150, 150);
-		ApplicationButton.setFont(new Font("맑은 고딕", Font.BOLD, 15));
+		c = new Color(64,126,219);
+		ApplicationButton.setBackground(c);
+		ApplicationButton.setForeground(Color.WHITE);
+		ApplicationButton.setBounds(30, 230, 530, 150);
+		ApplicationButton.setFont(new Font("맑은 고딕", Font.BOLD, 30));
 		ApplicationButton.addActionListener(this);
 		
-		JButton AppListButton = new JButton("신청 내역 확인");
+		RoundedButton AppListButton = new RoundedButton("신청 내역 확인");
 		add(AppListButton);
-		AppListButton.setBounds(350, 300, 150, 150);
-		AppListButton.setFont(new Font("맑은 고딕", Font.BOLD, 15));
+		AppListButton.setBackground(Color.WHITE);
+		AppListButton.setBounds(30, 430, 530, 50);
+		AppListButton.setFont(new Font("맑은 고딕", Font.BOLD, 25));
 		AppListButton.addActionListener(this);
+		
+		RoundedButton ChangeMemInfoButton = new RoundedButton("회원 정보 수정");
+		add(ChangeMemInfoButton);
+		ChangeMemInfoButton.setBackground(Color.WHITE);
+		ChangeMemInfoButton.setBounds(30, 500, 530, 50);
+		ChangeMemInfoButton.setFont(new Font("맑은 고딕", Font.BOLD, 25));
 		
 		JButton LogoutButton = new JButton("로그아웃");
 		add(LogoutButton);
@@ -55,7 +88,7 @@ public class MemberUI extends JFrame implements ActionListener{
 	
 	public void actionPerformed(ActionEvent e) {
 		String ActionCmd = e.getActionCommand();
-		if(ActionCmd.equals("신청하기")) {
+		if(ActionCmd.equals("돌봄 서비스 신청하기")) {
 			int ans = JOptionPane.showConfirmDialog(null,"신청을 생성하시겠습니까?","확인 메세지",JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE);
 			if(ans == 0){ // 신청 생성하기
 				MemAppInfoUI AppInfoWindow = new MemAppInfoUI();
