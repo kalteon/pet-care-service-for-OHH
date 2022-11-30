e9335e0e4e5e1abe131eedbec9ffdea30c3d7ff8
jungjihyung <wlgud3936@gmail.com>
2022-11-30 16:06:25 +0900

    Application Table 4개 추가

---
+++ src/db/ActiveTable.java
@@ -0,0 +1,59 @@
+package db;
+
+import java.util.Hashtable;
+
+import pd.application.Application;
+
+/*
+ * ActiveDM으로 불러온 Active Hashtable을 불러와서 보관하는 고유한 싱글톤 클래스.
+ * ActiveTable activetable = ActiveTable.getInstance() 형태로 불러오면 어디서든 같은 내용의 ForPaymentTable 사용가능.
+ */
+public class ActiveTable
+{
+    private static ActiveTable activetable;
+    private static Hashtable<String, Application> activehashtable;
+    private ActiveDM activeDM;
+    
+    private ActiveTable() {
+    	activeDM = new ActiveDM("ActiveDM");
+    }
+    
+    public static ActiveTable getInstance()
+    {
+        if (activetable == null)
+        {
+            synchronized(ActiveTable.class)
+            {
+                activetable = new ActiveTable();
+            }
+        }
+        return activetable;
+    }
+
+    //Complete로 상태 전환 및 이동
+    public void moveComplete(Application application) {
+    	activehashtable = activeDM.readObjectData();
+    	activehashtable.remove(application.getApplicationID());
+    	CompleteTable completetable = CompleteTable.getInstance();
+    	application.setState(3);
+    	completetable.putCompleteHashTable(application.getApplicationID(), application);
+    	activeDM.deletObjectData();
+		activeDM.writeObjectData(activehashtable);
+    }
+    //getter 파일이 없으면 null 반환
+	public Hashtable<String, Application> getActiveHashTable() {
+		activehashtable = activeDM.readObjectData();
+   		return activehashtable;
+	}
+	
+	//ActiveTable에 추가
+	public void putActiveHashTable(String applicationID, Application application) {
+		activehashtable = activeDM.readObjectData();
+		if(activehashtable == null)
+			activehashtable = new Hashtable<>();
+		activehashtable.put(applicationID, application);
+		activeDM.deletObjectData();
+		activeDM.writeObjectData(activehashtable);
+	}
+
+}
---
+++ src/db/CompleteTable.java
@@ -0,0 +1,50 @@
+package db;
+
+import java.util.Hashtable;
+
+import pd.application.Application;
+
+/*
+ * CompleteDM으로 불러온 Complete Hashtable을 불러와서 보관하는 고유한 싱글톤 클래스.
+ * CompleteTable completetable = CompleteTable.getInstance() 형태로 불러오면 어디서든 같은 내용의 CompleteTable 사용가능.
+ */
+public class CompleteTable
+{
+    private static CompleteTable completetable;
+    private static Hashtable<String, Application> completehashtable;
+    private CompleteDM completeDM;
+    
+    private CompleteTable() {
+    	completeDM = new CompleteDM("CompleteDM");
+    	completehashtable = new Hashtable<>();
+    }
+    
+    public static CompleteTable getInstance()
+    {
+        if (completetable == null)
+        {
+            synchronized(CompleteTable.class)
+            {
+                completetable = new CompleteTable();
+            }
+        }
+        return completetable;
+    }
+
+    //getter 파일이 없으면 null 반환
+	public Hashtable<String, Application> getCompleteHashTable() {
+		completehashtable = completeDM.readObjectData();
+   		return completehashtable;
+	}
+	
+	//Complete Table에 추가
+	public void putCompleteHashTable(String applicationID, Application application) {
+		completehashtable = completeDM.readObjectData();
+		if(completehashtable == null)
+			completehashtable = new Hashtable<>();
+		completehashtable.put(applicationID, application);
+		completeDM.deletObjectData();
+		completeDM.writeObjectData(completehashtable);
+	}
+
+}
